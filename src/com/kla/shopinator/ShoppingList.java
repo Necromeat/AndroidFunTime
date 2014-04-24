aa6b715d065e272b9f99a3fff4b9288629d70ca1
mcabe <mcabe@me.com>
2014-04-24 12:24:40 +0200

    Displays dummy shopping list in shopping list view

--- src/com/kla/shopinator/ShoppingList.java
+++ src/com/kla/shopinator/ShoppingList.java
@@ -1,5 +1,10 @@
 package com.kla.shopinator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import datamodels.ShoppingItemModel;
+import dbfunctions.Controller;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
@@ -9,6 +14,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.os.Build;
 
 public class ShoppingList extends Activity {
@@ -17,6 +24,20 @@ public class ShoppingList extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shopping_list);
+		
+		Controller con = new Controller();
+		ArrayList<String> dummyList = new ArrayList<String>();
+		List<ShoppingItemModel> temp = con.getShoppingList();
+		
+		for(ShoppingItemModel i : temp){
+			dummyList.add(i.getItemName()+i.getQuantity());
+		}
+		
+		ListView listViewHandle = (ListView)findViewById(R.id.shopping_list);
+		
+		ArrayAdapter<String> a = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dummyList);
+		
+		listViewHandle.setAdapter(a);
 	}
 
 	@Override
--- src/dbfunctions/Controller.java
+++ src/dbfunctions/Controller.java
@@ -9,14 +9,14 @@ public class Controller {
 	public List<ShoppingItemModel> getShoppingList(){
 		ArrayList<ShoppingItemModel> dummylist = new ArrayList<ShoppingItemModel>();
 		
-		dummylist.add(new ShoppingItemModel(1,"a"));
-		dummylist.add(new ShoppingItemModel(2,"b"));
-		dummylist.add(new ShoppingItemModel(3,"c"));
-		dummylist.add(new ShoppingItemModel(4,"d"));
-		dummylist.add(new ShoppingItemModel(5,"e"));
-		dummylist.add(new ShoppingItemModel(6,"f"));
-		dummylist.add(new ShoppingItemModel(7,"g"));
-		dummylist.add(new ShoppingItemModel(20,"k"));
+		dummylist.add(new ShoppingItemModel(10,"apple"));
+		dummylist.add(new ShoppingItemModel(2,"banana"));
+		dummylist.add(new ShoppingItemModel(6,"coke"));
+		dummylist.add(new ShoppingItemModel(1,"dog food"));
+		dummylist.add(new ShoppingItemModel(12,"eggs"));
+		dummylist.add(new ShoppingItemModel(1,"frozen peas"));
+		dummylist.add(new ShoppingItemModel(1,"garbage bags"));
+		dummylist.add(new ShoppingItemModel(1,"hagan daz"));
 		return dummylist;
 	}
 }
